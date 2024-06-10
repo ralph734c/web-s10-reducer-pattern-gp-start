0d2a4310c05cf9f306e090ed0de777a43bb6b837
Christopher Ralph <chris.734c@gmail.com>
2024-06-10 18:47:54 -0400

    Implemented all pieces of the Todo application using a reducer to manage and centralize state.

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,28 +1,67 @@
-import React from 'react'
+import React, { useReducer } from "react";
 
-export default function TodoForm() {
+const CHANGE_LABEL = "CHANGE_LABEL";
+const CHANGE_IS_COMPLETED = "CHANGE_IS_COMPLETED";
+
+const initialState = {
+  todoLabel: "",
+  todoIsCompleted: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_LABEL:
+      return { ...state, todoLabel: action.payload };
+    case CHANGE_IS_COMPLETED:
+      return { ...state, todoIsCompleted: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default function TodoForm({ createNewTodo }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const onLabelChange = ({ target: { value } }) => {
+    dispatch({ type: CHANGE_LABEL, payload: value });
+  };
+  const onIsCompletedChange = ({ target: { checked } }) => {
+    dispatch({ type: CHANGE_IS_COMPLETED, payload: checked });
+  };
+  const resetForm = () => {
+    dispatch({ type: CHANGE_LABEL, payload: "" });
+    dispatch({ type: CHANGE_IS_COMPLETED, payload: false });
+  };
+  const onNewTodo = (evt) => {
+    evt.preventDefault();
+    createNewTodo(state.todoLabel, state.todoIsCompleted);
+    resetForm();
+  };
   return (
-    <form id="todoForm">
+    <form id="todoForm" onSubmit={onNewTodo}>
       <h3>New Todo Form</h3>
-      <label><span>Todo label:</span>
+      <label>
+        <span>Todo label:</span>
         <input
-          type='text'
-          name='todoLabel'
-          placeholder='Type label'
+          onChange={onLabelChange}
+          value={state.todoLabel}
+          type="text"
+          name="todoLabel"
+          placeholder="Type label"
         />
       </label>
-      <label><span>Is completed:</span>
+      <label>
+        <span>Is completed:</span>
         <input
-          type='checkbox'
-          name='todoIsCompleted'
+          onChange={onIsCompletedChange}
+          checked={state.todoIsCompleted}
+          type="checkbox"
+          name="todoIsCompleted"
         />
       </label>
-      <label><span>Create todo:</span>
-        <input
-          type='submit'
-          value='Do it!'
-        />
+      <label>
+        <span>Create todo:</span>
+        <input type="submit" value="Do it!" />
       </label>
     </form>
-  )
+  );
 }
